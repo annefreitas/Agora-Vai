48e9b64acdf4752c10ece0c7032aadec503ef648
caniiggia <caniiggia2010@hotmail.com>
2017-11-09 21:03:14 -0400

    u.u

---
+++ app/cursor.py
@@ -0,0 +1,13 @@
+from app import app
+from .db_interface import Zelda
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'foundanies'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+# init MYSQL
+db = Zelda(app)
